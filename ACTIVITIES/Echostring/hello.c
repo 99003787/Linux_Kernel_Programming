9dabe8980f39e8b1419a62d91f2be5351ea17f54
99003787 <78854021+99003787@users.noreply.github.com>
2021-04-12 00:01:33 +0530

    Update hello.c

--- ACTIVITIES/Echostring/hello.c
+++ ACTIVITIES/Echostring/hello.c
@@ -1,1 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define __NR_testcall 401
+int main() {
+printf("hello world");
+int ret;
+char abc[100];
+ret=syscall(__NR_testcall,"kernel_prog",abc);
+printf("\nreversed string is %s\n", abc);
+if(ret<0)
+perror("testcall");
+return 0;
+}
 
