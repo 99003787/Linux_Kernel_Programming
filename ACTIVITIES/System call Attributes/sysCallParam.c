571ee870bdf17e8433b8c842dd062aa03ecea534
99003787 <78854021+99003787@users.noreply.github.com>
2021-04-12 10:10:37 +0530

    Update sysCallParam.c

--- ACTIVITIES/System call Attributes/sysCallParam.c
+++ ACTIVITIES/System call Attributes/sysCallParam.c
@@ -1,1 +1,7 @@
-
+#include<linux/kernel.h>
+#include<linux/syscalls.h>
+SYSCALL_DEFINE7(mysyscallparam,int,s,int,t,int,u,int,v)
+{
+	printk("This is Test call x=%d y=%d" ,s,t,u,v;
+	return s,t,u,v;
+}
