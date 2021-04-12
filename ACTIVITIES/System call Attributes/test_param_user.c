82603944321202c1f1ed3262831fc86f342783e9
99003787 <78854021+99003787@users.noreply.github.com>
2021-04-12 10:11:02 +0530

    Update test_param_user.c

--- ACTIVITIES/System call Attributes/test_param_user.c
+++ ACTIVITIES/System call Attributes/test_param_user.c
@@ -1,1 +1,12 @@
-
+#include <unistd.h>
+#include <stdio.h>
+#define __CR_testcall 399
+int main()
+{
+	int ret;
+        ret=syscall( __CR_testcall,60,70,80,90);
+	printf("%d",ret);
+	if(ret<0)
+	perror("testcall");
+	return 0;
+}
